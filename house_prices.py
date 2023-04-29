3f766cc79be8afceeb394286658f75bee0bcc6d9
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-04-29 00:27:03 -0700

    Include libraries

--- house_prices.py
+++ house_prices.py
@@ -1,1 +1,11 @@
-
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split, GridSearchCV, KFold, cross_val_score
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.linear_model import Lasso
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MaxAbsScaler
